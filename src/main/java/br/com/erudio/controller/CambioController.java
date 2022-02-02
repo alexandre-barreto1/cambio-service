0b7bfa8f132cd7b00bbbd8f183b1daf3c8d1b3a6
alexandre-dev <alexandre-dev@alexandre-dev>
2022-02-01 23:27:56 -0300

    Adicionando conexão com as tabelas de cambio-service

--- src/main/java/br/com/erudio/controller/CambioController.java
+++ src/main/java/br/com/erudio/controller/CambioController.java
@@ -1,6 +1,7 @@
 package br.com.erudio.controller;
 
 import br.com.erudio.model.Cambio;
+import br.com.erudio.repository.CambioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @RestController
 @RequestMapping("cambio-service")
@@ -17,11 +19,22 @@ public class CambioController {
     @Autowired
     private Environment environment;
 
+    @Autowired
+    private CambioRepository repository;
+
     @GetMapping(value = "/{amount}/{from}/{to}")
     public Cambio getCambio(@PathVariable("amount")BigDecimal amount,
                             @PathVariable("from")String from,
                             @PathVariable("to")String to) {
+        var cambio = repository.findByFromAndTo(from, to);
+        if(cambio == null) {
+            throw new RuntimeException("Currency Unsupported");
+        }
         var port = environment.getProperty("local.server.port");
-        return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
+        BigDecimal conversionFactory = cambio.getConversionFactor();
+        BigDecimal convertedValue = conversionFactory.multiply(amount);
+        cambio.setConvertedValue(convertedValue.setScale(2, RoundingMode.CEILING));
+        cambio.setEnviorment(port);
+        return cambio;
     }
 }
